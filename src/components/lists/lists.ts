2bb3550de3a9c9bb80d353c6ea09746abdfb9c29
dirk <doktordirk@outlook.de>
2019-11-02 18:01:08 +0100

    doc(mdc-list): separate avatar sample

--- src/components/lists/lists.ts
+++ src/components/lists/lists.ts
@@ -2,7 +2,6 @@ import { MdcDialog, IMdcListItemClickEvent } from '../../bridge/index';
 
 export class Lists {
   private dense = true;
-  private avatar = false;
   private dialog: MdcDialog;
 
   // List item
